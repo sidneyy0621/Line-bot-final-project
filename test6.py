459f26b39bad13cf017b91a4331ed975642db60b
CHEN,WEI-CHUN <c112156210@nkust.edu.tw>
2025-01-01 02:15:43 +0800

    Delete test6.py

--- test6.py
+++
@@ -1,9 +0,0 @@
-import os
-import google.generativeai as generativeai
-from dotenv import load_dotenv
-
-load_dotenv()
-
-generativeai.configure(api_key=os.getenv("KEY"))
-response = generativeai.GenerativeModel('gemini-2.0-flash-exp').generate_content("who are you?")
-print(response.text)
