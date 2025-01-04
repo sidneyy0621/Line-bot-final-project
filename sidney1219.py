fc57a5843374c50a9305826177972fd2a0b67bb1
“sidneyy0621” <C112156210@nkust.edu.tw>
2025-01-04 14:05:48 +0800

    刪除檔案

--- sidney1219.py
+++
@@ -1,9 +0,0 @@
-import os
-import google.generativeai as generativeai
-from dotenv import load_dotenv
-
-load_dotenv()
-
-generativeai.configure(api_key=os.getenv("your_api_key_here"))
-response = generativeai.GenerativeModel("gemini-2.0-flash-exp").generate_content("Do u nkow NKUST?")
-print(response.text)
